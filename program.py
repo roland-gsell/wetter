bbd98fe120e4aed3f600b01fd653f7b05e384102
Roland Gsell <roland.gsell@gmx.net>
2018-05-15 09:48:44 +0200

    initial commit

---
+++ program.py
@@ -0,0 +1,41 @@
+import requests
+import bs4
+import collections
+
+
+city = collections.namedtuple('cities',
+                              'name, link')
+
+
+def main():
+    print_header()
+    html = get_html()
+    cities = get_cities(html)
+    for city in cities:
+        print(city.name)
+
+
+def get_cities(html):
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    city_list = []
+    for entry in soup.find(class_='entrys').find_all('a', href=True):
+        c = city(name=entry.text.strip(), link=entry['href'])
+        city_list.append(c)
+    return city_list
+
+
+def get_html():
+    response = requests.get('http://www.wetter.at/locationSearch/3500')
+    print(response)
+    return response.text
+
+
+def print_header():
+    print('-----------------------------')
+    print('         Wetter-App')
+    print('-----------------------------')
+    print()
+
+
+if __name__ == '__main__':
+    main()
